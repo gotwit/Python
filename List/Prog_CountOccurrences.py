f38899beae76b583487f7fb56f9245e01dc62827
Srikanth Paratala MoganRaju <v-srpara@microsoft.com>
2020-06-14 11:35:11 -0700

    DS

--- List/Prog_CountOccurrences.py
+++ List/Prog_CountOccurrences.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def countOccurrences(lst, x):
     count = 0
     for e in lst:
@@ -6,8 +9,31 @@ def countOccurrences(lst, x):
     return count
 
 
+# count
+def countOccurrences2(lst, x):
+    return lst.count(x)
+
+
+# counter
+def countOccurrences3(lst, x):
+    d = Counter(lst)
+    return d[x]
+
+
 if __name__ == '__main__':
     lst = [15, 6, 7, 10, 12, 20, 10, 28, 10]
     x = int(input("Enter a number to find occurrences: "))
     occurrence = countOccurrences(lst, x)
+    print(f'{lst} : {x} occurs {occurrence} times\n')
+
+if __name__ == '__main__':
+    lst = [15, 6, 7, 10, 12, 20, 10, 28, 10]
+    x = int(input("Enter a number to find occurrences: "))
+    occurrence = countOccurrences2(lst, x)
+    print(f'{lst} : {x} occurs {occurrence} times\n')
+
+if __name__ == '__main__':
+    lst = [15, 6, 7, 10, 12, 20, 10, 28, 10]
+    x = int(input("Enter a number to find occurrences: "))
+    occurrence = countOccurrences3(lst, x)
     print(f'{lst} : {x} occurs {occurrence} times\n')
