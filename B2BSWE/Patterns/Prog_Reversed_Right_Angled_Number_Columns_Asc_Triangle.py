db60298c43bd9cefa6c0df2dbf1f7e2b5e171322
solouniverse <solouniverse19@gmail.com>
2022-05-11 14:13:10 +0530

    Pattern

--- B2BSWE/Patterns/Prog_Reversed_Right_Angled_Number_Columns_Asc_Triangle.py
+++ B2BSWE/Patterns/Prog_Reversed_Right_Angled_Number_Columns_Asc_Triangle.py
@@ -3,9 +3,17 @@ n = int(input("Enter no of test runs: "))
 while n > 0:
     rows = int(input("Enter no of rows: "))
     columns = int(input("Enter no of columns: "))
-    
+
+    for row in range(rows, 0, -1):
+        for col in range(row):
+            print(col + 1, end=" ")
+        print()
+    n = n - 1
+
+"""     
     for row in range(rows):
         for col in range(0, columns - row):
             print(col + 1, end = " ")
-        print()
+        print() 
     n = n - 1
+    """
--- B2BSWE/Patterns/Prog_Reversed_Right_Angled_Number_Desc_Triangle.py
+++ B2BSWE/Patterns/Prog_Reversed_Right_Angled_Number_Desc_Triangle.py
@@ -6,7 +6,8 @@ while n > 0:
 
     for row in range(rows, 0, -1):
         # for col in range(columns, columns - row, -1):
-        for col in range(columns - row, columns):
+        # for col in range(columns - row, columns):
+        for col in range(row):
             print(row, end=" ")
         print()
     n = n - 1
