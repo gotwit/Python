d854b60d3a65cc5393690d1638238f10a34e4716
solouniverse <solouniverse19@gmail.com>
2022-06-22 09:03:16 +0530

    Loops

--- Notes/ControlFlow.py
+++ Notes/ControlFlow.py
@@ -1,4 +1,4 @@
-for i in range(10):
+""" for i in range(10):
     print(i, end=' ')
 
     
@@ -52,3 +52,31 @@ list(range(1, 11))
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for(int i = 0; i < n; i++)
 
+ """
+""" 
+2: (2,2) =>  
+3: (2,3) =>  2 
+4: (2,4) =>  2 3 
+5: (2,5) =>  2 3 4 
+6: (2,6) =>  2 3 4 5 
+7: (2,7) =>  2 3 4 5 6 
+8: (2,8) =>  2 3 4 5 6 7 
+9: (2,9) =>  2 3 4 5 6 7 8 
+ """
+""" 
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x, ' & is not a prime number')
+            break
+    else:
+        print(n, 'is a prime number')
+ """
+""" 
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f"Found an even number {num}")
+        continue
+    print("Found an odd number %(n)d" % {'n': num})
+ """
+ 
