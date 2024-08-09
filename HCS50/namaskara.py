89a7cb8f2a9fe65b067b7bd7ba4f94c2860e8d1e
gotwit <gotwitb@gmail.com>
2024-08-09 13:28:23 +0530

    hello

--- HCS50/namaskara.py
+++ HCS50/namaskara.py
@@ -1,1 +1,20 @@
-print("namaskara")
+# print("What's your name? ")
+# input()
+# print("namaskara")
+
+# input("What's your name? ")
+# print("Namaskara Sri")
+
+
+# name = input("What's your name? ")
+# print(name)
+# print("Namaskara")
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hi to user
+print("Namaskara", name)
+
+print("Hi " + name)
+
