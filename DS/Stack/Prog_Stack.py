e06f78d518450a179aea4373f9f6658280050edb
gotwit <gotwitb@gmail.com>
2024-09-19 14:27:44 +0530

    Fixed Stack

--- DS/Stack/Prog_Stack.py
+++ DS/Stack/Prog_Stack.py
@@ -11,7 +11,7 @@
 
 class Stack:
 
-    
+
     def __init__(self, capacity=0):
         self.lst = []
         self.capacity = capacity
@@ -36,14 +36,6 @@ class Stack:
             return None
 
 
-    # def top(self):
-    #     if self.top != -1:
-    #         index = self.top
-    #         return self.lst[index]
-    #     else:
-    #         return None
-
-
     def peek(self):
         if self.top != -1:
             index = self.top
@@ -65,49 +57,60 @@ class Stack:
 
 
 size = int(input("Enter the size of stack: "))
-stk = Stack(size)
 
-# print("Size of stack: ", stk.capacity)
+if size > 0:
+    stk = Stack(size)
 
-n = int(input("Enter the number of iterations: "))
+    # print("Size of stack: ", stk.capacity)
 
-while n > 0:
-    status = int(input("""
-Enter 0 to print stack
-Enter 1 to push
-Enter 2 to pop
-Enter 3 to peek
-Enter 4 to check stack is empty
-Enter 5 to check stack is full
-"""))
+    n = int(input("Enter the number of iterations: "))
 
-    match status:
-        case 0:
-            print("Stack :", stk.show())
-        case 1:
-            value = int(input("Enter a value: "))
-            lst = stk.push(value)
-            if lst is not None:
-                print(lst)
-            else:
-                print("Stack size is incorrect")
-        case 2:
-            item = stk.pop()
-            if item is not None:
-                print("Removed ", item)
-            else:
-                print("Stack has no elements to remove")
-        case 3:
-            item = stk.peek()
-            if item is not None:
-                print("Peek ", item)
-            else:
-                print("Stack has no elements to show")
-        case 4:
-            isEmpty = stk.isEmpty()
-            print("Stack is empty" if isEmpty else "Stack is not empty")
-        case 5:
-            isFull = stk.isFull()
-            print("Stack is full" if isFull else "Stack is not full")
+    while n > 0:
+        try:
+            status = int(input("""
+        Enter 0 to print stack
+        Enter 1 to push
+        Enter 2 to pop
+        Enter 3 to peek
+        Enter 4 to check stack is empty
+        Enter 5 to check stack is full
+        """))
 
-    n -= 1
+            match status:
+                case 0:
+                    print("Stack :", stk.show())
+                case 1:
+                    value = int(input("Enter a value: "))
+                    lst = stk.push(value)
+                    if lst is not None:
+                        print(lst)
+                    else:
+                        print("Please increase stack size")
+                case 2:
+                    item = stk.pop()
+                    if item is not None:
+                        print("Removed value", item)
+                    else:
+                        print("Stack has no elements to remove")
+                case 3:
+                    item = stk.peek()
+                    if item is not None:
+                        print("Peek value: ", item)
+                    else:
+                        print("Stack has no elements to show")
+                case 4:
+                    isEmpty = stk.isEmpty()
+                    print("Stack is empty" if isEmpty else "Stack is not empty")
+                case 5:
+                    isFull = stk.isFull()
+                    print("Stack is full" if isFull else "Stack is not full")
+                case _:
+                    print("Please enter valid choice")
+
+            n -= 1
+        except ValueError:
+            print("Please enter valid choice")
+    else:
+        print("Please enter the valid number of iterations")
+else:
+    print("Please enter the valid size of stack")
